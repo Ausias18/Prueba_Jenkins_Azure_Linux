b1a698b559410056836f5bf3b846c30263055695
Ausias <62700889+Ausias18@users.noreply.github.com>
2020-04-30 07:00:49 +0000

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -34,7 +34,7 @@ ip_configuration {
 }
 
 resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm-2"
+  name                  = "${var.prefix}-vm"
   location              = azurerm_resource_group.main.location
   resource_group_name   = azurerm_resource_group.main.name
   network_interface_ids = [azurerm_network_interface.main.id]
