8092f854ede6b9f9cf909e0ef313f6699da09beb
Ausias <62700889+Ausias18@users.noreply.github.com>
2020-05-05 17:04:54 +0200

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -28,7 +28,8 @@ resource "azurerm_network_interface" "main" {
 
 ip_configuration {
     name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.internal.id
+    
+  subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id = azurerm_public_ip.main.id
   }
