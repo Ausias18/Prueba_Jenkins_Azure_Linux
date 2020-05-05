667017de184e62f9194f0775de3408d41220d192
Ausias <62700889+Ausias18@users.noreply.github.com>
2020-05-05 17:11:39 +0200

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -21,6 +21,14 @@ resource "azurerm_subnet" "internal" {
   address_prefix       = "10.0.2.0/24"
 }
 
+resource "azurerm_public_ip" "main" {
+  name                = "SantaluciaPIP"
+  location            = "westeurope"
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+  domain_name_label   = "santalucia-azurerm-resource"
+}
+
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
   location            = azurerm_resource_group.main.location
@@ -30,16 +38,8 @@ ip_configuration {
     name                          = "testconfiguration1"
     subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
-   # public_ip_address_id = azurerm_public_ip.main.id
+    public_ip_address_id = azurerm_public_ip.main.id
   }
-}
-
-resource "azurerm_public_ip" "main" {
-  name                = "SantaluciaPIP"
-  location            = "westeurope"
-  resource_group_name = azurerm_resource_group.main.name
-  allocation_method   = "Static"
-  domain_name_label   = "santalucia-azurerm-resource"
 }
 
 resource "azurerm_network_security_group" "main" {
