5444bbf99a479dc2a3bed3e71097f0c06e9a6861
Ausias <62700889+Ausias18@users.noreply.github.com>
2020-05-05 12:17:00 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+provider "azure" {
+#  publish_settings = "${file("credentials.publishsettings")}"
+}
 provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x. 
   # If you are using version 1.x, the "features" block is not allowed.
