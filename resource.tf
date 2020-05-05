3aa19cdb23fbeb1173602d49db67a6be2d63c6ac
Ausias <62700889+Ausias18@users.noreply.github.com>
2020-05-05 15:11:04 +0200

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -25,6 +25,7 @@ resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
+  public_ip_address_id = azurerm_network_interface.main.id
 
 ip_configuration {
     name                          = "testconfiguration1"
