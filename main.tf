bc2a7247af3f18403aefbe2a268d624edc718b57
Ausias <62700889+Ausias18@users.noreply.github.com>
2020-05-05 12:18:31 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
 provider "azure" {
 #  publish_settings = "${file("credentials.publishsettings")}"
+  subscription_id = "2de9d718-d170-4e29-af3b-60c30e449b3c"
+  tenant_id = "ece33831-9bc7-4217-a330-2082dfa1a525"
+  client_id = "15055b44-7983-416f-823f-99c4e348dfe7"
+  client_secret = "7b7af9ec-d750-4ed0-9d2e-cb7a30ec3575"
+  features {}
 }
 provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x. 
