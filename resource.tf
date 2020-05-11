8ce09fba6db8b302fa88f6009f0a4a8206cce27a
Ausias <62700889+Ausias18@users.noreply.github.com>
2020-05-11 11:08:17 +0200

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -70,9 +70,21 @@ resource "azurerm_network_security_group" "main" {
         destination_address_prefix = "*"
     }
   
+     security_rule {
+        name                       = "Access-http"
+        priority                   = 220
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+  
       security_rule {
         name                       = "InternetAccess"
-        priority                   = 220
+        priority                   = 230
         direction                  = "Outbound"
         access                     = "Allow"
         protocol                   = "*"
