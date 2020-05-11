bf2079b96e6c918b9fa06f4e23cb16b691994fc4
Ausias <62700889+Ausias18@users.noreply.github.com>
2020-05-11 17:30:18 +0200

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -270,7 +270,7 @@ storage_image_reference {
    } 
   
    os_profile_secrets { 
-     source_vault_id = "azurerm_key_vault.main.id" 
+     #source_vault_id = "azurerm_key_vault.main.id" 
   
      vault_certificates { 
        certificate_url   = "azurerm_key_vault_certificate.main.id" 
