be39d2e763ef4399d433fe053b1398867a287548
Ausias <62700889+Ausias18@users.noreply.github.com>
2020-05-11 16:52:42 +0200

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -166,6 +166,7 @@ resource "azurerm_key_vault" "main" {
  resource "azurerm_key_vault_certificate" "main" { 
    name      = "${var.prefix}-cert" 
    vault_uri = "azurerm_key_vault.main.vault_uri" 
+   vault_id = "azurerm_key_vault.main.vault_id"
   
    certificate_policy { 
      issuer_parameters { 
