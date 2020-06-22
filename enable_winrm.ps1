93cf28828ebd46944f60727b80b3e4b7607a1da2
Ausias <62700889+Ausias18@users.noreply.github.com>
2020-06-22 18:08:41 +0200

    Update enable_winrm.ps1

--- enable_winrm.ps1
+++ enable_winrm.ps1
@@ -1,8 +1,7 @@
 <powershell>
 
-Restart-Service -Name WinRM
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1' -UseBasicParsing -Outfile $env:TEMP\ConfigureRemotingForAnsible.ps1
 
-. "$($env:TEMP)\ConfigureRemotingForAnsible.ps1" -CertValidityDays 3650
+. "$($env:TEMP)\ConfigureRemotingForAnsible.ps1"
 
 </powershell>
