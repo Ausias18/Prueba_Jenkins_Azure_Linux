0ac5cc7cea64d69fb6e8c61c5fbd34c8d7c86f26
Ausias <62700889+Ausias18@users.noreply.github.com>
2020-06-23 11:26:12 +0200

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -327,13 +327,13 @@ storage_image_reference {
   resource "azurerm_virtual_machine_extension" "main" {
     name            = "hostname"
     virtual_machine_id  =  azurerm_virtual_machine.main.id
-    publisher       = "Microsoft.Compute"
-    type            = "CustomScriptExtension"
-    type_handler_version    = "1.9"
+    publisher       = "Microsoft.Azure.Extensions"
+    type            = "CustomScript"
+    type_handler_version    = "2.0"
  
     settings = <<SETTINGS
     {
-         "commandToExecute": "PowerShell.exe $env:SystemDrive\\ConfigureRemotingForAnsible.ps1"
+         "commandToExecute": "PowerShell.exe $env:SystemDrive\ConfigureRemotingForAnsible.ps1"
     }
     SETTINGS
     }
