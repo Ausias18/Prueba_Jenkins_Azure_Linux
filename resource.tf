2f3901a2612a2d44bfb73a328ccef58b14fbf58e
Ausias <62700889+Ausias18@users.noreply.github.com>
2020-06-24 09:59:32 +0200

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -115,116 +115,7 @@ resource "azurerm_network_interface_security_group_association" "main" {
  data "azurerm_client_config" "current" {
 }
 
-#-------------------Creación de Certificado -------------------------------
-#resource "azurerm_key_vault" "main" {
-#  name                = "keyvaultcertmain"
-#  location            = azurerm_resource_group.main.location
-#  resource_group_name = azurerm_resource_group.main.name
-#  tenant_id           = "ece33831-9bc7-4217-a330-2082dfa1a525"
-
-# sku_name = "standard"
-
-#  access_policy {
-#    tenant_id = "ece33831-9bc7-4217-a330-2082dfa1a525"
-#    object_id = data.azurerm_client_config.current.object_id
-
-#    certificate_permissions = [
-#      "create",
-#      "delete",
-#      "deleteissuers",
-#      "get",
-#      "getissuers",
-#      "import",
-#      "list",
-#      "listissuers",
-#      "managecontacts",
-#      "manageissuers",
-#      "setissuers",
-#      "update",
-#    ]
-#
-#    key_permissions = [
-#      "backup",
-#      "create",
-#      "decrypt",
-#      "delete",
-#      "encrypt",
-#      "get",
-#      "import",
-#      "list",
-#      "purge",
-#      "recover",
-#      "restore",
-#      "sign",
-#      "unwrapKey",
-#      "update",
-#      "verify",
-#      "wrapKey",
-#    ]
-#
-#    secret_permissions = [
-#      "backup",
-#      "delete",
-#      "get",
-#      "list",
-#      "purge",
-#      "recover",
-#      "restore",
-#      "set",
-#    ]
-#  }
-#} 
-#  
-#resource "azurerm_key_vault_certificate" "main" {
-#  name         = "generated-cert"
-#  key_vault_id = azurerm_key_vault.main.id
-#   
-#  
-#   certificate_policy { 
-#     issuer_parameters { 
-#       name = "Self" 
-#     } 
-#  
-#     key_properties { 
-#       exportable = true 
-#       key_size   = 2048 
-#       key_type   = "RSA" 
-#       reuse_key  = true 
-#     } 
-#  
-#    lifetime_action { 
-#       action { 
-#         action_type = "AutoRenew" 
-#       } 
-#  
-#       trigger { 
-#         days_before_expiry = 30 
-#       } 
-#     } 
-#  
-#     secret_properties { 
-#       content_type = "application/x-pkcs12" 
-#     } 
-#  
-#     x509_certificate_properties { 
-#     extended_key_usage = ["1.3.6.1.5.5.7.3.1"]
-#       
-#     key_usage = [ 
-#         "cRLSign", 
-#         "dataEncipherment", 
-#         "digitalSignature", 
-#         "keyAgreement", 
-#         "keyCertSign", 
-#         "keyEncipherment", 
-#       ] 
-#  
-#       subject            = "CN=${azurerm_network_interface.main.private_ip_address}" 
-#       validity_in_months = 12 
-#     } 
-#  } 
-# }
-# ------------------------------------------------------------------------------ 
-
+#--------INSTALLING Machine from Image -------------------------
 resource "azurerm_virtual_machine" "main" {
   name                  = "${var.prefix}-vm"
   location              = azurerm_resource_group.main.location
@@ -242,29 +133,6 @@ storage_image_reference {
     id = "/subscriptions/2de9d718-d170-4e29-af3b-60c30e449b3c/resourceGroups/santalucia-imagenes-packer/providers/Microsoft.Compute/images/ws2016-winrm-packer"
 }
 
-# resource "azurerm_image" "main" {
-#  name                = "packer-image"
-#  location            = azurerm_resource_group.main.location
-#  resource_group_name = azurerm_resource_group.main.name
-    
-# Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
-
-# Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
-
-#storage_image_reference {
-#    publisher = "MicrosoftWindowsServer"
-#    offer     = "WindowsServer"
-#    sku       = "2016-Datacenter"
-#    version   = "latest"
-# }
-#  storage_os_disk {
-#    name              = "myosdisk1"
-#    caching           = "ReadWrite"
-#    create_option     = "FromImage"
-#    managed_disk_type = "Standard_LRS"
-#  }
   os_profile {
     computer_name  = "Prueba"
     admin_username = "arqsis"
@@ -275,52 +143,10 @@ storage_image_reference {
      provision_vm_agent = true 
      winrm {
        protocol = "http"
-      # protocol        = "https" 
-      # certificate_url = "azurerm_key_vault_certificate.main.id" 
-       #certificate_url = "${azurerm_key_vault.main.vault_uri}secrets/${azurerm_key_vault_certificate.main.name}/${azurerm_key_vault_certificate.main.version}"
            } 
                             } 
-
- # provisioner "remote-exec" {
- #       connection {
- #       type = "winrm"
- #       port = 5985
- #       https    = false
- #       insecure = true
- #       host = "santalucia-azurerm-resource.westeurope.cloudapp.azure.com" #azurerm_public_ip.main.id public_ip_address_id
- #       user     = "arqsis"
- #       password = "Password1234!"
- #       timeout     = "4m" # ----> TIMEOUT PARAMETER ADDED
- #   }
- #     inline = [         
- #         "PowerShell.exe $env:SystemDrive\\ConfigureRemotingForAnsible.ps1"
- #     ]
- # }
   }
-
-  
- #  os_profile_secrets { 
-   #source_vault_id = "azurerm_key_vault.main.id"
-  # source_vault_id =  "/subscriptions/2de9d718-d170-4e29-af3b-60c30e449b3c/resourceGroups/santalucia-resources/providers/Microsoft.KeyVault/vaults/keyvaultcertmain"
-     
-   #vault_certificates { 
-   #certificate_url   = "azurerm_key_vault_certificate.main.id" 
-  # certificate_url   = "${azurerm_key_vault.main.vault_uri}secrets/${azurerm_key_vault_certificate.main.name}/${azurerm_key_vault_certificate.main.version}"
-
-   #certificate_store = "My" 
-   #  } 
-  # } 
- #} 
-  
-  # os_profile_windows_config {
- #   provision_vm_agent = "true"
- #   enable_automatic_upgrades = "true"
- #   winrm {
- #     protocol        = "http" 
- #     }
- # }
-  
- # }
+#----------------------------------------------------------------------------
   
 #--------INSTALLING ./ConfigureRemotingForAnsible.ps1 -------------------------
   resource "azurerm_virtual_machine_extension" "main" {
